4ff5dbe84b47ef88e1003cbaea8748bda9a320fd
vikramsinwal <vikramsinwal@gmail.com>
2026-01-14 23:35:02 +0530

    email template with selection 0.1

--- src/app/actions/sendMail.ts
+++ src/app/actions/sendMail.ts
@@ -5,51 +5,52 @@ import { sendMail } from "@/lib/3p-api/php-mailer";
 import { verifyCaptcha } from "@/lib/verifyCaptcha";
 import { getEmailTemplate } from "@/lib/emailTemplateResolver";
 import {
-    saveEmail,
-    markEmailSent,
-    markEmailFailed,
+  saveEmail,
+  markEmailSent,
+  markEmailFailed,
 } from "@/lib/services/mail.service";
 
 export async function sendMailAction(formData: unknown) {
-    const data = contactSchema.parse(formData);
-    let fromEmail = data.fromEmail;
-    let fromName = data.fromName;
+  const data = contactSchema.parse(formData);
+  let fromEmail = data.fromEmail;
+  let fromName = data.fromName;
 
-    // 🔐 captcha check
-    const isHuman = await verifyCaptcha(data.token);
-    if (!isHuman) throw new Error("Captcha failed");
+  // 🔐 captcha check
+  const isHuman = await verifyCaptcha(data.token);
+  if (!isHuman) throw new Error("Captcha failed");
 
-    // Generate email HTML
-    const html = getEmailTemplate(data);
-    // 💾 save email to DB
-    const emailDoc = await saveEmail({
-        to: data.email,
-        fromEmail,
-        fromName,
-        subject: data.subject,
-        html,
-        templateKey: data.templateKey,
+  // Generate email HTML
+  const html = getEmailTemplate(data);
+  // 💾 save email to DB
+  const emailDoc = await saveEmail({
+    to: data.email,
+    fromEmail,
+    fromName,
+    subject: data.subject,
+    html,
+    message: data.message,
+    templateKey: data.templateKey,
+  });
+
+  try {
+    // Send email
+    await sendMail({
+      to: data.email,
+      subject: data.subject,
+      html,
+      fromEmail,
+      fromName,
     });
 
-    try {
-        // Send email
-        await sendMail({
-            to: data.email,
-            subject: data.subject,
-            html,
-            fromEmail,
-            fromName,
-        });
+    //Mark as SENT
+    await markEmailSent(emailDoc._id.toString());
+  } catch (error: any) {
+    // Mark as FAILED
+    await markEmailFailed(
+      emailDoc._id.toString(),
+      error?.message || "Mail sending failed"
+    );
 
-        //Mark as SENT
-        await markEmailSent(emailDoc._id.toString());
-    } catch (error: any) {
-        // Mark as FAILED
-        await markEmailFailed(
-            emailDoc._id.toString(),
-            error?.message || "Mail sending failed"
-        );
-
-        throw error;
-    }
+    throw error;
+  }
 }
